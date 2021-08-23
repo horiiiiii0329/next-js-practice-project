038fce076eaf362e7153949848d999b891345fd3
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-23 21:57:18 +0900

    header

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>AirBnb</title>
+        <title>AirMnM</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Lets build Airbnb</h1>
+      <Header />
     </div>
   );
 }
