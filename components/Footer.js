ce2018c16afd6738d8a415b40dab3b4e412457d3
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-24 23:15:53 +0900

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,40 @@
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 px-32 py-14 bg-gray-100 gap-y-10 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5>ABOUT</h5>
+        <p>how Airbnb works</p>
+        <p>This is not a real site</p>
+        <p>LMAOOOOOO</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Lxuxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5>ABOUT</h5>
+        <p>how Airbnb works</p>
+        <p>This is not a real site</p>
+        <p>LMAOOOOOO</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Lxuxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5>ABOUT</h5>
+        <p>how Airbnb works</p>
+        <p>This is not a real site</p>
+        <p>LMAOOOOOO</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Lxuxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5>ABOUT</h5>
+        <p>how Airbnb works</p>
+        <p>This is not a real site</p>
+        <p>LMAOOOOOO</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Lxuxe</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
