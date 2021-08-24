5a4f6e62760f3b26de04d434e44cd25b8ac1260e
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-24 22:19:41 +0900

    card

--- components/Banner.js
+++ components/Banner.js
@@ -8,8 +8,11 @@ function Banner() {
         layout="fill"
         objectFit="cover"
       />
-      <div className="absolute">
-        <p>Not sure where to go ?</p>
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go ?</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          flexible
+        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -11,6 +12,33 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        <section className="pt-5">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+
+        {exploreData?.map((item) => (
+          <SmallCard
+            key={item.img}
+            img={item.img}
+            distance={item.distance}
+            location={item.location}
+          />
+        ))}
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
